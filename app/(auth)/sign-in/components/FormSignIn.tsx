939b0db0294dd15b469dc028a840552fc4d18a6f
iczky <fiqra22@gmail.com>
2024-06-22 12:25:03 +0700

    feat: add sign in page 1. create a generic form to be use in any component 2. using yup as validation schema

---
+++ app/(auth)/sign-in/components/FormSignIn.tsx
@@ -0,0 +1,47 @@
+import GenericForm from "@/components/Form/GenericForm";
+import * as Yup from "yup";
+
+const FormSignIn = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email address").required("Required!!"),
+    password: Yup.string().required("Password required!!"),
+  });
+
+  const handleSubmit = (values: any) => {
+    console.log("Sign in", values);
+  };
+
+  const fields = [
+    {
+      id: "email",
+      label: "Email",
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+    },
+    {
+      id: "password",
+      label: "Password",
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+    },
+  ];
+
+  return (
+    <GenericForm
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      fields={fields}
+      onSubmit={handleSubmit}
+      formTitle="Sign in to EventHive"
+    />
+  );
+};
+
+export default FormSignIn;
---
+++ components/Form/GenericForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+interface FormField {
+  id: string;
+  label: string;
+  name: string;
+  type: string;
+  placeholder?: string;
+  as?: string;
+}
+
+interface GenericFormProps {
+  initialValues: any;
+  validationSchema: Yup.ObjectSchema<any>;
+  onSubmit: (values: any) => void;
+  fields: FormField[];
+  formTitle: string;
+}
+
+const GenericForm: React.FC<GenericFormProps> = ({
+  initialValues,
+  validationSchema,
+  onSubmit,
+  fields,
+  formTitle,
+}) => {
+  return (
+    <section className="px-4 py-5 flex flex-col items-center text-black">
+      <h1 className="text-3xl font-bold">{formTitle}</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}>
+        <Form className="pt-4 w-full">
+          {fields.map((field) => (
+            <div key={field.id} className="flex flex-col gap-3 pt-2">
+              <label
+                htmlFor={field.id}
+                className="text-sm font-light text-left pt-4">
+                {field.label}
+              </label>
+              <Field
+                id={field.id}
+                name={field.name}
+                type={field.type}
+                placeholder={field.placeholder}
+                as={field.as || "input"}
+                className="text-black px-4 py-3 rounded-box"
+              />
+            </div>
+          ))}
+          <div className="pt-5">
+            <button
+              type="submit"
+              className="bg-primary text-white font-extralight text-lg py-2 px-4 mt-8 rounded-box">
+              Submit
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </section>
+  );
+};
+
+export default GenericForm;
